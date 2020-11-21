c28da61ed3b6d3c4fc7828115f0371923333718a
Nicolas <nicolas.kerneis29@gmail.com>
2020-11-21 18:45:52 +0100

    ajout des packages, police et fragment bandeau du haut

---
+++ app/src/main/java/com/example/latabledesgourmands/cuisinier/acceuilCuisinier.java
@@ -0,0 +1,16 @@
+package com.example.latabledesgourmands.cuisinier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.latabledesgourmands.R;
+
+public class acceuilCuisinier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil_cuisinier);
+    }
+}

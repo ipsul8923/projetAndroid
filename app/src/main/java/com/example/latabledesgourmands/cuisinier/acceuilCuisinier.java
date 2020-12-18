a4862f31c02a88017db93f4ba12005971548385a
Nicolas <nicolas.kerneis29@gmail.com>
2020-12-18 10:45:42 -0500

    add logo back

--- app/src/main/java/com/example/latabledesgourmands/cuisinier/acceuilCuisinier.java
+++ app/src/main/java/com/example/latabledesgourmands/cuisinier/acceuilCuisinier.java
@@ -5,24 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.latabledesgourmands.R;
 import com.example.latabledesgourmands.cuisinier.creerMaTable.creerMaTable;
 import com.example.latabledesgourmands.cuisinier.créerMonMenu.creerMonMenu;
 import com.example.latabledesgourmands.cuisinier.gererMesMenus.gererMesMenus;
 import com.example.latabledesgourmands.cuisinier.gérerMesTables.gererMesTables;
+import com.example.latabledesgourmands.demarrageapplication.ChoixCuisinierInvite;
 
 public class acceuilCuisinier extends AppCompatActivity {
-
+    Button acceuilButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceuil_cuisinier);
+        acceuilButton = findViewById(R.id.logo);
+        acceuilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChoixCuisinierInviteActivity();
+            }
+        });
     }
+    private void startChoixCuisinierInviteActivity(){
+        Intent intent = new Intent(this, ChoixCuisinierInvite.class);
+        startActivity(intent);
+    }
+
+
     private void startCreerMaTableActivity(){
         Intent intent = new Intent(this, creerMaTable.class);
         startActivity(intent);
     }
+
+
     private void startGererMesTablesActivity(){
         Intent intent = new Intent(this, gererMesTables.class);
         startActivity(intent);
